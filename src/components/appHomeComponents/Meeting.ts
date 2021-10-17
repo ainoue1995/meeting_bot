31e7bfc441e452ec52e5f29a8b4a5735d1ead292
ainoue1995 <inoueakio@terakoyapro.net>
2021-10-18 02:45:52 +0900

    typeの追加と dateのformat

--- src/components/appHomeComponents/Meeting.ts
+++ src/components/appHomeComponents/Meeting.ts
@@ -13,6 +13,15 @@ export const meetingDetails = (
       return prev
     }, ``)
   console.log('users', users)
+  const formattedDate = (rowDate: Date) => {
+    const year = rowDate.getFullYear()
+    const month = ('0' + (rowDate.getMonth() + 1)).slice(-2)
+    const date = ('0' + rowDate.getDate()).slice(-2)
+    const hour = rowDate.getHours().toString().padStart(2, '0')
+    const minutes = rowDate.getMinutes().toString().padStart(2, '0')
+
+    return `${year}年${month}月${date}日 ${hour}:${minutes}`
+  }
 
   return [
     {
@@ -20,7 +29,9 @@ export const meetingDetails = (
       block_id: `meeting_details_block_id-${meeting.id}`,
       text: {
         type: 'mrkdwn',
-        text: `*#${topic}*\n\n*${meeting.date.toLocaleString()}* *at* *${meetingPlace}*\n参加メンバー: ${users}`
+        text: `*#${topic}*\n\n*${formattedDate(
+          meeting.date
+        )}* *at* *${meetingPlace}*\n参加メンバー: ${users}`
       }
       // accessory: {
       //   type: 'button',
